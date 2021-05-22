e31f2c163d1fd628bfa415a01aac7b14ec663c1f
Harvey <harvey0379@163.com>
2021-05-22 18:14:54 +0800

    drag event demo

--- src/Board.jsx
+++ src/Board.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Square from './Square';
 import Knight from './Knight';
 import { moveKnight, canMoveKnight } from './Game';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import BoardSquare from './BoardSquare';
 
 function handleSquareClick(toX, toY) {
   if (canMoveKnight(toX, toY)) {
@@ -9,23 +12,21 @@ function handleSquareClick(toX, toY) {
   }
 }
 
-function renderSquare(i, [knightX, knightY]) {
+function renderPiece(x, y, [knightX, knightY]) {
+  if (x === knightX && y === knightY) {
+    return <Knight />;
+  }
+}
+
+function renderSquare(i, knightPosition) {
   const x = i % 8;
   const y = Math.floor(i / 8);
-  const isKnightHere = x === knightX && y === knightY;
-  const black = (x + y) % 2 === 1;
-  const piece = isKnightHere ? <Knight /> : null;
 
   return (
-    <div
-      key={i}
-      style={{
-        width: '100px',
-        height: '100px',
-      }}
-      onClick={() => handleSquareClick(x, y)}
-    >
-      <Square black={black}>{piece}</Square>
+    <div key={i} style={{ width: '12.5%', height: '12.5%' }}>
+      <BoardSquare x={x} y={y}>
+        {renderPiece(x, y, knightPosition)}
+      </BoardSquare>
     </div>
   );
 }
@@ -37,17 +38,19 @@ export default function Board({ knightPosition }) {
   }
 
   return (
-    <div
-      style={{
-        width: '1000px',
-        height: '100%',
-        padding: '100px',
-        display: 'flex',
-        flexWrap: 'wrap',
-        boxSizing: 'border-box',
-      }}
-    >
-      {squares}
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div
+        style={{
+          width: '1000px',
+          height: '100%',
+          padding: '100px',
+          display: 'flex',
+          flexWrap: 'wrap',
+          boxSizing: 'border-box',
+        }}
+      >
+        {squares}
+      </div>
+    </DndProvider>
   );
 }
--- src/Knight.jsx
+++ src/Knight.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { ItemTypes } from './Constants';
+import { useDrag, DragPreviewImage } from 'react-dnd';
+import knightImage from './head.png';
 
 export default function Knight() {
-  return <span style={{ fontSize: '50px' }}>♘</span>;
+  const [{ isDragging }, drag, preview] = useDrag(() => ({
+    type: ItemTypes.KNIGHT,
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+  return (
+    <>
+      <DragPreviewImage connect={preview} src={knightImage} />
+      <span
+        ref={drag}
+        style={{
+          fontSize: '50px',
+          opacity: isDragging ? 0.5 : 1,
+          fontWeight: 'bold',
+          cursor: 'move',
+        }}
+      >
+        ♘
+      </span>
+    </>
+  );
 }
